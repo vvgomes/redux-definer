ce18ae3148a4d41fb61794ae0e182bc326fd0901
Vinicius Gomes <vinicius.vieira.gomes@gmail.com>
2016-10-07 01:31:06 -0400

    Simplifies the tests

--- test/define.reducer.test.js
+++ test/define.reducer.test.js
@@ -3,28 +3,25 @@ import { expect } from "chai";
 import { stub } from "sinon";
 
 describe("reducer()", () => {
-  const actionHandlers = {
-    incrementCounter: stub().returns(1)
-  };
-
+  const actionHandlers = { incrementCounter: stub().returns(1) };
   const reducer = defineReducer(actionHandlers);
 
   it("calls appropriate handler for action", () => {
     const state = 0;
     const action = { type: "incrementCounter" };
-    expect(reducer(state, action)).to.be.eq(1);
+    expect(reducer(state, action)).eq(1);
   });
 
   it("returns unchanged state when no handler is found", () => {
     const state = 0;
     const action = { type: "unknown" };
-    expect(reducer(state, action)).to.be.eq(0);
+    expect(reducer(state, action)).eq(0);
   });
 
   it("returns unchanged state when action has no type", () => {
     const state = 0;
     const action = {};
-    expect(reducer(state, action)).to.be.eq(0);
+    expect(reducer(state, action)).eq(0);
   });
 });
 
